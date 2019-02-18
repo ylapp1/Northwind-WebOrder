3006a9ed767816b057ef741a27691ebe26e32325
a <a>
2019-02-18 13:57:34 +0100

    Erweiterung 2

--- src/web/WebServer.js
+++ src/web/WebServer.js
@@ -46,10 +46,17 @@ class WebServer
         // Dynamic pages
         this.express.get("/", this.indexResponse.bind(this));
         this.express.get("/orders", this.ordersResponse.bind(this));
+        this.express.get("/customers", this.customersResponse.bind(this));
+        this.express.get("/workers", this.workersResponse.bind(this));
+        this.express.get("/providers", this.providersResponse.bind(this));
+        this.express.get("/dateRange", this.dateRangeResponse.bind(this));
 
         // Static paths
+        this.express.use("/css", express.static(__dirname + "/resources/pages"));
         this.express.use("/javascript", express.static(__dirname + "/resources/pages"));
-        this.express.use("/jquery", express.static(__dirname + "/external"));
+        this.express.use("/jquery", express.static(__dirname + "/../../node_modules/jquery/dist"));
+        this.express.use("/jquery-ui", express.static(__dirname + "/../../node_modules/jquery-ui-dist"));
+        this.express.use("/flatpickr", express.static(__dirname + "/../../node_modules/flatpickr/dist"));
     }
 
 
@@ -68,48 +75,126 @@ class WebServer
         });
     }
 
-    ordersResponse(_request, _response)
+    /**
+     * Responds to a request with the result of a query
+     *
+     * @param _request The request
+     * @param _response The response
+     * @param String _query The query to execute
+     */
+    queryResponse(_request, _response, _query)
     {
-        this.getOrders().then(function(_result){
-            _response.json(_result);
+        this.databaseConnection.query(_query, function(_error, _result){
+            if (! _error) _response.json(_result);
         });
     }
 
-    getOrders()
+    /**
+     * Responds to a request to the "/orders" route.
+     *
+     * @param _request The request
+     * @param _response The response
+     */
+    ordersResponse(_request, _response)
     {
         // This query was the initial task
         //let sqlQuery = "SELECT * FROM artikel";
 
-        // This query selects some data
-        let sqlQuery = `
-          SELECT *, (Gesamtwert + Frachtkosten) AS Gesamtwarenwert FROM
-          (
-            SELECT
-              BestellNr,
-              kunden.Firma AS Kunde,
-              CONCAT(personal.Vorname, " ", personal.Nachname) AS \`Vor- und Nachname des Sachbearbeiters\`,
-              BestellDatum,
-              lieferanten.Firma AS Lieferant,
-              ROUND(Frachtkosten, 2) AS Frachtkosten,
-              ROUND(SUM(EinzelPreis * Anzahl * (1 - Rabatt)), 2) AS Gesamtwert
-            FROM bestellungen
-            JOIN bestelldetails USING (BestellNr)
-            JOIN kunden USING(KundenCode)
-            JOIN personal USING(PersonalNr)
-            JOIN lieferanten ON VersandUeber = lieferanten.LieferantenNr
-            GROUP BY (BestellNr)
-          ) AS result
-        `;
+        this.queryResponse(
+            _request,
+            _response,
+            `SELECT *, (Gesamtwert + Frachtkosten) AS Gesamtwarenwert FROM
+             (
+               SELECT
+                 BestellNr,
+                 kunden.Firma AS Kunde,
+                 CONCAT(personal.Vorname, " ", personal.Nachname) AS \`Vor- und Nachname des Sachbearbeiters\`,
+                 DATE_FORMAT(BestellDatum, "%Y-%m-%d") AS BestellDatum,
+                 lieferanten.Firma AS Lieferant,
+                 ROUND(Frachtkosten, 2) AS Frachtkosten,
+                 ROUND(SUM(EinzelPreis * Anzahl * (1 - Rabatt)), 2) AS Gesamtwert
+               FROM bestellungen
+               JOIN bestelldetails USING (BestellNr)
+               JOIN kunden USING(KundenCode)
+               JOIN personal USING(PersonalNr)
+               JOIN lieferanten ON VersandUeber = lieferanten.LieferantenNr
+               GROUP BY (BestellNr)
+             ) AS result;
+           `);
+    }
 
-        let self = this;
-        return new Promise(function(_resolve, _reject){
+    /**
+     * Responds to a request to the "/customers" route.
+     *
+     * @param _request The request
+     * @param _response The response
+     */
+    customersResponse(_request, _response)
+    {
+        this.queryResponse(
+            _request,
+            _response,
+            `SELECT
+               Kunden.Firma AS Kunde
+             FROM Kunden
+             ORDER BY Kunde ASC;`
+        );
+    }
 
-            self.databaseConnection.query(sqlQuery, function(_error, _result){
-                if (_error) _reject(_error);
-                else _resolve(_result);
-            });
+    /**
+     * Responds to a request to the "/workers" route.
+     *
+     * @param _request The request
+     * @param _response The response
+     */
+    workersResponse(_request, _response)
+    {
+        this.queryResponse(
+            _request,
+            _response,
+            `SELECT
+               CONCAT(personal.Vorname, ' ', personal.Nachname) AS Sachbearbeiter
+             FROM Personal
+             ORDER BY Sachbearbeiter ASC;
+            `
+        );
+    }
 
-        });
+    /**
+     * Responds to a request to the "/providers" route.
+     *
+     * @param _request The request
+     * @param _response The response
+     */
+    providersResponse(_request, _response)
+    {
+        this.queryResponse(
+            _request,
+            _response,
+            `SELECT
+               lieferanten.Firma AS Lieferant
+             FROM Lieferanten
+             ORDER BY Lieferant ASC;`
+        );
+    }
+
+    /**
+     * Responds to a request to the "/dateRange" route.
+     *
+     * @param _request The request
+     * @param _response The response
+     */
+    dateRangeResponse(_request, _response)
+    {
+        this.queryResponse(
+            _request,
+            _response,
+            `SELECT
+               DATE_FORMAT(MIN(BestellDatum), "%Y-%m-%d") AS MinBestellDatum,
+               DATE_FORMAT(MAX(BestellDatum), "%Y-%m-%d") AS MaxBestellDatum
+             FROM Bestellungen;
+            `
+        );
     }
 }
 
--- src/web/resources/pages/index.js
+++ src/web/resources/pages/index.js
@@ -2,9 +2,10 @@
 $(document).ready(function(){
 
     fetchDataRows().then(function(_dataRows){
-        updateResultTable(_dataRows);
+        initializeResultTable(_dataRows);
+        renderResultTable();
+        initializeFilters();
     });
-
 });
 
 /**
@@ -20,18 +21,30 @@ function fetchDataRows()
 }
 
 /**
- * Updates the result table.
+ * Initialize the results and filters for the result table.
  */
-function updateResultTable(_resultRows)
+function initializeResultTable(_resultRows)
+{
+    $("table#resultTable").data("rows", _resultRows);
+    $("table#resultTable").data("filters", {});
+}
+
+/**
+ * Renders the result table.
+ */
+function renderResultTable()
 {
     var resultTable = $("table#resultTable");
+    resultTable.empty();
 
-    if (_resultRows.length == 0) $(resultTable).html("Keine Einträge vorhanden");
+    var resultRows = applyFilters(resultTable.data("rows"), resultTable.data("filters"));
+
+    if (resultRows.length == 0) $(resultTable).html("Keine Einträge vorhanden");
     else
     { // Create the table headers
 
         var headerRow = $("<tr>");
-        for (var propertyName in _resultRows[0])
+        for (var propertyName in resultRows[0])
         {
             headerRow.append($("<th>").html(propertyName));
         }
@@ -39,9 +52,9 @@ function updateResultTable(_resultRows)
         resultTable.append($(headerRow));
     }
 
-    for (var i = 0; i < _resultRows.length; i++)
+    for (var i = 0; i < resultRows.length; i++)
     {
-        var resultRow = _resultRows[i];
+        var resultRow = resultRows[i];
         var tableRow = $("<tr>");
 
         // Iterate over the property names (the column titles) of the result row
@@ -56,4 +69,131 @@ function updateResultTable(_resultRows)
 
         $(resultTable).append($(tableRow));
     }
+}
+
+/**
+ * Applies the filters to the order data rows and returns only the rows that match the filters.
+ */
+function applyFilters(_rows, _filters)
+{
+    var resultRows = [];
+
+    for (var i = 0; i < _rows.length; i++)
+    {
+        var row = _rows[i];
+        var rowMatchesFilters = true;
+
+        // Iterate over the property names (the column titles) of the result row
+        for (var columnName in _filters) {
+            if (row.hasOwnProperty(columnName))
+            {
+                if (_filters[columnName] !== "all")
+                {
+                    var columnValue = row[columnName];
+                    if (columnValue !== _filters[columnName])
+                    {
+                        rowMatchesFilters = false;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Check the date range
+        var orderDate = Date.parse(row.BestellDatum);
+        if (_filters.dateFrom && orderDate < _filters.dateFrom) rowMatchesFilters = false;
+        if (_filters.dateTo && orderDate > _filters.dateTo) rowMatchesFilters = false;
+
+        if (rowMatchesFilters) resultRows.push(row);
+    }
+
+    return resultRows;
+}
+
+/**
+ * Initializes the available list filters.
+ */
+function initializeFilters()
+{
+    initializeComboBox("select#customerFilter", "customers", "Kunde");
+    initializeComboBox("select#workerFilter", "workers", "Sachbearbeiter");
+    initializeComboBox("select#providerFilter", "providers", "Lieferant");
+    initializeDateRangeFilter("input#dateRangeFilter", "dateRange");
+}
+
+/**
+ * Initializes a combo box.
+ *
+ * @param String _comboBoxSelector The combo box selector
+ * @param String _dataURL The url from which the values for the combo box can be fetched
+ * @param String _optionValueKey The key of the values in the data rows
+ */
+function initializeComboBox(_comboBoxSelector, _dataURL, _optionValueKey)
+{
+    var comboBox = $(_comboBoxSelector);
+    comboBox.empty();
+    comboBox.append($("<option value=\"all\">Alle</option>"));
+
+    $.get(_dataURL, function(_data, _status){
+        for (var i = 0; i < _data.length; i++)
+        {
+            var optionValue = _data[i][_optionValueKey];
+            comboBox.append($("<option value=\"" + optionValue + "\">" + optionValue + "</option>"));
+        }
+    });
+
+    comboBox.on("change", function(_event){
+        var comboBox = $(_event.target);
+        var selectedOption = $(_event.target).find("option:selected")[0];
+
+        var resultTable = $("table#resultTable");
+        resultTable.data("filters")[comboBox.data("column")] = $(selectedOption).val();
+        renderResultTable();
+    });
+}
+
+/**
+ * Initializes the date range filter.
+ *
+ * @param String _inputFieldSelector The input field selector
+ * @param String _dateURL The URL from which the min and max date can be fetched
+ */
+function initializeDateRangeFilter(_inputFieldSelector, _dataURL)
+{
+    var inputField = $(_inputFieldSelector);
+    inputField.val = null;
+
+    $.get(_dataURL, function(_data, _status){
+
+        var minDate = Date.parse(_data[0].MinBestellDatum);
+        var maxDate = Date.parse(_data[0].MaxBestellDatum);
+
+        // Initialize the date range picker
+        $(_inputFieldSelector).flatpickr({
+            minDate: minDate,
+            maxDate: maxDate,
+            defaultDate: [ minDate, maxDate ],
+            mode: "range",
+            formatDate: function(_selectedDate){
+                return _selectedDate.toLocaleDateString("de-DE");
+            },
+            locale: {
+                rangeSeparator: " bis "
+            },
+            onClose: function(_selectedDates){
+                var resultTable = $("table#resultTable");
+
+                var minDate = _selectedDates[0];
+                var maxDate = _selectedDates[1];
+                minDate.setHours(minDate.getHours() + 2);
+                maxDate.setHours(maxDate.getHours() + 2);
+
+                resultTable.data("filters")["dateFrom"] = minDate;
+                resultTable.data("filters")["dateTo"] = maxDate;
+
+                renderResultTable();
+            }
+        });
+
+    });
 }
